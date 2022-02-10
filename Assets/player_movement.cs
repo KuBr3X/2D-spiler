1b50e870456afbcb83b172839c4eb2213c5de33b
Jakub <dampsjakub5@gmail.com>
2022-02-10 12:11:25 +0100

    first upload

---
+++ Assets/player_movement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour{
+
+    public float Speed;
+    public float JumpPower;
+
+    private Rigidbody2D RB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        Vector2 movement = new Vector2(0, RB.velocity.y);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            movement.x = -Speed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+          movement.x = Speed;  
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            RB.AddForce(new Vector2(0,JumpPower));
+        }
+
+        RB.velocity = movement;
+    }
+    
+}
